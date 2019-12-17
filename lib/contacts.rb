1c49fb309b6ebb9c850c9bc9fffb3d0b591c592f
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-17 20:11:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,10 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts["Freddy Mercury"].each do |key, value|
-  if key == :favorite_ice_cream_flavors
-    value.delete_if {|ice_cream| ice_cream == "strawberry"}
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
   end
-
-end
 end
